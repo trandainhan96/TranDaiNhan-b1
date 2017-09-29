4cf88231635f3a0cd43ec2d7da3cb668ac678feb
trandainhan96 <dainhantran160396@gmail.com>
2017-09-29 22:35:45 +0700

    update

--- BAI03/Program.cs
+++ BAI03/Program.cs
@@ -8,8 +8,24 @@ namespace BAI03
 {
     class Program
     {
+        private static string strIP;		//input from user
+        private static int inpMask;	//input from user
+
+        private static List<int> ip;
+        private static List<int> mask;
+        private static List<int> netID;
+        private static List<int> broadcast;
+
+        private static void input()
+        {
+            Console.Write("Nhap dia chi IP: ");
+            strIP = Console.ReadLine();
+            Console.Write("Nhap dia chi broadcast: ");
+            inpMask = Convert.ToInt32(Console.ReadLine());
+        }
         static void Main(string[] args)
         {
+            input();
         }
     }
 }
