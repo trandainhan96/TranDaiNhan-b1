f08a139022616f0c7b6caed69bbd8e0bafe03be2
trandainhan96 <dainhantran160396@gmail.com>
2017-09-29 11:36:51 +0700

    init project

---
+++ BAI 01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace BAI_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach(IPAddress ip in host.AddressList)
+            {
+                Console.WriteLine(ip.ToString());
+            }
+            
+            
+        }
+    }
+}
---
+++ BAI2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace BAI2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap ten mien hoac IP: ");
+            string host = Console.ReadLine();
+
+            IPHostEntry e = Dns.GetHostEntry(host);
+            Console.WriteLine("HostName: {0}", e.HostName);
+
+            foreach(string s in e.Aliases)
+                Console.WriteLine("\t Alias: {0 \n",s);
+
+            foreach (IPAddress i in e.AddressList)
+                Console.WriteLine("\t IP: {0},i");
+        }
+    }
+}
